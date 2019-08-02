d55fc3f71d96e89423a6efe7fa2530cccfb1c447
Chetan Rane <chethran@in.ibm.com>
2019-08-02 14:47:42 +0530

    initial commit

---
+++ routes/jda.js
@@ -0,0 +1,78 @@
+const {Country} = require('../data');
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+router.get('/tabs', (req, res) => {
+    const item = req.params.item;
+
+    let itemList = [{
+            label: 'Country',
+            value: 'Country'
+        },
+        {
+            label: 'Location',
+            value: 'Location'
+        },
+        {
+            label: 'Customer',
+            value: 'Customer'
+        },
+        {
+            label: 'Resoruce',
+            value: 'Resoruce'
+        }
+    ];
+
+    res.send(itemList);
+})
+
+router.get('/tabs/:item', (req, res) => {
+    const item = req.params.item;
+
+    let itemList = [],
+        childItem = [],
+        childItems = [],
+        childItem1 = [],
+        childItems1 = [];
+    const hasChild = {Customer : [1,4,8], Country : [2,6,9], Location : [4,7,8], Resoruce:[]}
+    for (let i = 1; i < 11; i++) {
+        for (let j = 1; j < 3; j++) {
+            for (let k = 1; k < 3; k++) {
+                childItem1 = {
+                    label: `${item} ${i}`,
+                    value: `${i}.${j}.${k}`,
+                    checked: false
+                }
+                childItems1.push(childItem1)
+            }
+            childItem = {
+                label: `${item} Group ${i}`,
+                value: `${i}.${j}`,
+                checked: false,
+                children: j % 2 ? childItems1 : []
+            }
+            childItems1 = [];
+            childItems.push(childItem)
+        }
+        productItem = {
+            label: `${item} Category ${i}`,
+            value: `${i}`,
+            checked: false,
+            children: i % 2 ? childItems : []
+        }
+        childItems = [];
+        itemList.push(productItem);
+    }
+    console.log(Country);
+    res.send(Country);
+})
+
+module.exports = router;
